4550023d29521a98bcda7833c92779d6c74eb294
lucasgodt <lucasgodt@gmail.com>
2017-12-02 20:57:13 -0200

    temporarios

--- addcsv.py
+++ addcsv.py
@@ -4,6 +4,6 @@ import csv
 
 def addLine(fields,filename):
 
-    with open(filename, 'a', newline='') as f:
+    with open('tmp'+filename, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(fields)
