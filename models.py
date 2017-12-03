c648fcc34001f8a01f07f946e1c552d2fdab7a09
lucasgodt <lucasgodt@gmail.com>
2017-12-03 03:25:08 -0200

    testandoDB6

--- models.py
+++ models.py
@@ -12,7 +12,6 @@ import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Tracks(db.Model):
