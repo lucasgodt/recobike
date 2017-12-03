a5de19966b53b87a460b2557dba6cc5c5c743782
lucasgodt <lucasgodt@gmail.com>
2017-12-03 02:14:26 -0200

    tentativa

--- models.py
+++ models.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
 class Tracks(db.Model):
+    id_ride = db.Column(db.Integer, primary_key=True)
     id_android = db.Column(db.String(80), primary_key=False)
     id_track = db.Column(db.String(80), unique=True, nullable=False)
     rating = db.Column(db.Integer, unique=False, nullable=True)
@@ -23,6 +24,7 @@ class Tracks(db.Model):
         return '<User %r>' % self.username
     
 class TrackPoints(db.Model):
+    id_point = db.Column(db.Integer, primary_key=True)
     id_track = db.Column(db.String(80), primary_key=False)
     latitude = db.Column(db.Integer, unique=False, nullable=False)
     longitude = db.Column(db.Integer, unique=False, nullable=False)
