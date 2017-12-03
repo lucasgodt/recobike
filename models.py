6312a215b5a45a0c7cec798aa3f19800b1cce6b6
lucasgodt <lucasgodt@gmail.com>
2017-12-03 04:06:20 -0200

    bancodedados

--- models.py
+++ models.py
@@ -28,8 +28,8 @@ class TrackPoints(db.Model):
     __tablename__ = 'TrackPoints';
     id_point = db.Column(db.Integer, primary_key=True, autoincrement = True)
     id_track = db.Column(db.String(80), primary_key=False)
-    latitude = db.Column(db.Float(precision=10), unique=False, nullable=False)
-    longitude = db.Column(db.Float(precision=10), unique=False, nullable=False)
+    latitude = db.Column(db.Float(precision='4,8'))
+    longitude = db.Column(db.Float(precision='4,8'))
 
     def __repr__(self):
         return '<Track %r>' % self.id_track
